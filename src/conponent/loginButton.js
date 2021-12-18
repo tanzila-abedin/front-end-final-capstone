1b1edd4dcd87729ba04e26a76fb96a1e30cc6353
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-12-18 18:08:39 +0200

    Create a login button to take to auth) login page

---
+++ src/conponent/loginButton.js
@@ -0,0 +1,14 @@
+import { useAuth0 } from '@auth0/auth0-react';
+
+const LoginButton = () => {
+  const { loginWithRedirect } = useAuth0();
+  return (
+
+    <button type="button" onClick={() => loginWithRedirect()}>
+      Log In
+    </button>
+
+  );
+};
+
+export default LoginButton;
