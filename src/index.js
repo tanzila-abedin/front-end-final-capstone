d3632078d331c525903d46b462749a275d5cbcc4
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-12-18 19:02:02 +0200

    Wrap auth0 provider around provider component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Auth0Provider } from '@auth0/auth0-react';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './redux/store';
+
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+const audience = process.env.REACT_APP_AUTH0_AUDIENCE;
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Auth0Provider
+    domain={domain}
+    clientId={clientId}
+    redirectUri={window.location.origin}
+    audience={audience}
+  >
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Auth0Provider>,
   document.getElementById('root'),
 );
