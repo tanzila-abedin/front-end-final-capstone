35a1760ea90ea6bb3e2f3ee848a1b1bad962e0af
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-12-19 09:38:52 +0200

    Create api call to POST review , GET reviews for each laptop, and DELETE reviews created by a user

--- src/apiCall.js
+++ src/apiCall.js
@@ -18,3 +18,40 @@ export const getUsers = async () => {
   const users = await response.json();
   return users;
 };
+
+export const postReview = (data, accesstoken) => fetch(reviewsEndpoint, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    authorization: `Bearer ${accesstoken}`,
+  },
+  body: JSON.stringify(data),
+});
+
+export const getLaptopReviews = async (accessToken, id) => {
+  const url = 'http://localhost:3000';
+  const laptopReviewsEndpoint = `${url}/reviews/${id}`;
+  const response = await fetch(laptopReviewsEndpoint, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+  const laptopReviews = await response.json();
+  return laptopReviews;
+};
+
+export const deleteReviews = async (accessToken, id) => {
+  const url = 'http://localhost:3000';
+  const laptopReviewsEndpoint = `${url}/reviews/${id}`;
+  const response = await fetch(laptopReviewsEndpoint, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+  const laptopReviews = await response.json();
+  return laptopReviews;
+};
