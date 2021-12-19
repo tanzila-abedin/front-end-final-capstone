a8c57e3ea8e3752176f2f3e6e49518951d9ca0c9
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-12-19 09:53:36 +0200

    call public home page route and protected single page route ,  all reviews page route inside app component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import LaptopList from './conponent/laptopList';
+import Navigate from './conponent/navigate';
+import ProtectedRoute from './conponent/protectedRoute';
+import SingleLaptop from './conponent/singleLaptop';
+import userRatingList from './conponent/userRatingList';
+// import Footer from './footer';
+
 function App() {
   return (
-    <div className="App">
-      <h1> Hello world</h1>
-    </div>
+    <BrowserRouter>
+      <Navigate />
+      <Switch>
+        <Route exact path="/" component={LaptopList} />
+        <ProtectedRoute path="/singleLaptop/:id" component={SingleLaptop} />
+        <ProtectedRoute path="/AllRatings" component={userRatingList} />
+      </Switch>
+      {/* <Footer /> */}
+    </BrowserRouter>
   );
 }
 
