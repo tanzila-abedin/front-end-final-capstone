16c6a77ad9a685d2a7ca8b2904d33cb2d6716924
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-12-20 09:59:13 +0200

    Add welcome link paths

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import Navigate from './component/navigate';
 import ProtectedRoute from './auth0/protectedRoute';
 import SingleLaptop from './container/singleLaptop';
 import userRatingList from './container/userRatingList';
+import Welcome from './component/welcome';
 
 function App() {
   return (
     <BrowserRouter>
       <Navigate />
       <Switch>
-        <Route exact path="/" component={LaptopList} />
+        <Route exact path="/" component={Welcome} />
+        <Route exact path="/models" component={LaptopList} />
         <ProtectedRoute path="/singleLaptop/:id" component={SingleLaptop} />
         <ProtectedRoute path="/AllRatings" component={userRatingList} />
       </Switch>
