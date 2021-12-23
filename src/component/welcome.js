73fff50923ca6069875ab4e655f63c174518da21
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-12-23 20:25:19 +0200

    Add Bootstrap classes for styling

--- src/component/welcome.js
+++ src/component/welcome.js
@@ -1,10 +1,12 @@
 import { Link } from 'react-router-dom';
 
 const Welcome = () => (
-  <>
-    <h1>Welcome to DEVLAPS</h1>
-    <Link className="nav-text font-weight-normal" to="models">See laptop</Link>
-  </>
+
+  <div className="text-center welcome-container">
+    <h1 className="p-0 ">Welcome to DEVLAPS</h1>
+    <Link className="nav-text font-weight-normal btn bm-burger-bars all-btns  text-light" to="models">See laptop</Link>
+  </div>
+
 );
 
 export default Welcome;
