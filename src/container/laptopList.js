4f6981adcb7cc733a0b6cedfcfb99930adb0650b
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-12-23 20:27:39 +0200

    Add carousel to display list of laptop models horizontally

--- src/container/laptopList.js
+++ src/container/laptopList.js
@@ -1,7 +1,9 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import Carousel from 'react-elastic-carousel';
 import { fetchLaptopApi } from '../redux/actions/creator';
 import LaptopCard from '../component/laptopCard';
+import '../styles/index.css';
 
 const LaptopList = () => {
   const { laptops } = useSelector((state) => state);
@@ -12,11 +14,29 @@ const LaptopList = () => {
     );
   }, []);
 
+  const breakPoints = [
+    {
+      width: 500, itemsToShow: 1,
+    },
+    {
+      width: 768, itemsToShow: 2,
+    },
+    {
+      width: 1200, itemsToShow: 3,
+    },
+
+  ];
+
   return (
-    <div>
-      { laptops.laptops
-        ? laptops.laptops.map((laptop) => <LaptopCard key={laptop.id} laptopProp={laptop} />)
-        : <div>loading </div>}
+
+    <div className="pt-5">
+      <h2 className="text-center">LATEST MODELS</h2>
+      <p className="text-center text-muted">Please select a Laptop Model</p>
+      <Carousel breakPoints={breakPoints}>
+        { laptops.laptops
+          ? laptops.laptops.map((laptop) => <LaptopCard key={laptop.id} laptopProp={laptop} />)
+          : <div>loading </div>}
+      </Carousel>
     </div>
   );
 };
