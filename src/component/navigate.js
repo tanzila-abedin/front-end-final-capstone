68ed381bbedd8a0e5a615b118be00a7ea3154563
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-12-23 20:24:48 +0200

    Add menu component for hamburger navbar to display

--- src/component/navigate.js
+++ src/component/navigate.js
@@ -1,5 +1,6 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import { Link } from 'react-router-dom';
+import { slide as Menu } from 'react-burger-menu';
 import LoginButton from '../auth0/loginButton';
 import LogoutButton from '../auth0/logoutButton';
 import { getUsers, postUser } from '../apiCall';
@@ -18,27 +19,27 @@ const Navigate = () => {
   }
   return isAuthenticated ? (
     <>
-      <LogoutButton />
-      {' '}
-      <Link className="nav-text font-weight-normal" to="/"> Home</Link>
-      <Link className="nav-text font-weight-normal" to="/models">Laptops</Link>
+      <Menu className="overflow-hidden">
+        <h4>
+          {' '}
+          Hello
+          {' '}
+          {user.nickname}
+        </h4>
+        <Link className="menu-item" to="/">HOME</Link>
+        <Link className="menu-item" to="/models">LAPTOPS</Link>
 
-      <Link to="/AllRatings">Reviews</Link>
-      <Link to="/">
-        {' '}
-        Hello
-        {' '}
-        {user.nickname}
-
-      </Link>
-      {' '}
-      {' '}
+        <Link className="menu-item" to="/AllRatings">REVIEWS</Link>
+        <LogoutButton className="btn btn-primary menu-item" />
+      </Menu>
     </>
   ) : (
     <>
-
-      <LoginButton />
-      <Link className="nav-text font-weight-normal" to="/">Home</Link>
+      <Menu isOpen={false}>
+        <LoginButton className="menu-item btn all-btns" />
+        <Link className="menu-item" to="/">HOME</Link>
+        <Link className="menu-item" to="/models">LAPTOPS</Link>
+      </Menu>
     </>
   );
 };
