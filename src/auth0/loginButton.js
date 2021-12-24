292b89ba75665fbc5fe0b08b1a219b351da2f791
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-12-24 11:03:41 +0200

    Change login and logout button styles

--- src/auth0/loginButton.js
+++ src/auth0/loginButton.js
@@ -4,7 +4,7 @@ const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
   return (
 
-    <button type="button" onClick={() => loginWithRedirect()}>
+    <button type="button" className="all-btns btn bm-burger-bars text-white" onClick={() => loginWithRedirect()}>
       Log In
     </button>
 
--- src/auth0/logoutButton.js
+++ src/auth0/logoutButton.js
@@ -4,7 +4,7 @@ const LogoutButton = () => {
   const { logout } = useAuth0();
 
   return (
-    <button type="button" onClick={() => logout({ returnTo: window.location.origin })}>
+    <button type="button" className="all-btns btn bm-burger-bars text-white" onClick={() => logout({ returnTo: window.location.origin })}>
       Log Out
     </button>
 
