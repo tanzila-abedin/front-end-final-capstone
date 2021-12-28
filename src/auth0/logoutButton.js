14c4789ecb478a8d8de166be3b9e9e882e8013fe
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-12-28 11:19:22 +0200

    Redirect user to home page after logging out

--- src/auth0/logoutButton.js
+++ src/auth0/logoutButton.js
@@ -1,13 +1,15 @@
 import { useAuth0 } from '@auth0/auth0-react';
+import { Link } from 'react-router-dom';
 
 const LogoutButton = () => {
   const { logout } = useAuth0();
 
   return (
-    <button type="button" className="all-btns btn bm-burger-bars text-white" onClick={() => logout({ returnTo: window.location.origin })}>
-      Log Out
-    </button>
-
+    <Link className="menu-item item-hover" to="/">
+      <button type="button" className="all-btns btn bm-burger-bars text-white" onClick={() => logout()}>
+        Log Out
+      </button>
+    </Link>
   );
 };
 
